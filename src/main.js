fea46a375449bcb383c137a5101bcbd536e12c21
Diego Victor <diegovictorgonzaga@gmail.com>
2022-10-28 16:19:16 -0300

    Register responses and its colors

---
+++ src/main.js
@@ -0,0 +1,39 @@
+const responses = [
+  {
+    text: "YES, JUST GO",
+    color: "#1e96fc",
+  },
+  {
+    text: "NOPE",
+    color: "#fb3640",
+  },
+  {
+    text: "MAYBE?",
+    color: "#fdca40",
+  },
+  {
+    text: "I DON'T RECOMMEND",
+    color: "#7765e3",
+  },
+  {
+    text: "IF NO THEN YES",
+    color: "#6d5a72",
+  },
+  {
+    text: "WHY NOT?",
+    color: "#d138bf",
+  },
+  {
+    text: "WHATEVER",
+    color: "#f37748",
+  },
+  {
+    text: "TRY TOMORROW",
+    color: "#333333",
+  },
+  {
+    text: 'MY DOG SAID "YES"',
+    color: "#f87575",
+  },
+];
+
